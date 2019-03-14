553cd664e479d635315509df32e81f5d56114389
Kasper <kasper@desktop-gkogd7o.home>
2019-03-14 12:35:50 +0100

    lab 2 init

---
+++ SapalaKasper/cw02/zad1/generator.c
@@ -0,0 +1,5 @@
+#include "generator.h"
+
+void generate(const unsigned int num_records, const unsigned int bite_num, const char *file_name) {
+
+}
---
+++ SapalaKasper/cw02/zad1/generator.h
@@ -0,0 +1,6 @@
+#ifndef GENERATOR_LIB_H
+#define GENERATOR_LIB_H
+
+void generate(const unsigned int num_records, const unsigned int bite_num, const char *file_name);
+
+#endif

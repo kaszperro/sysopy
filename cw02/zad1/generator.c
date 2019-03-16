4d0151d5e5116fb021fc29e16db68d4a3e8c3a59
Kasper Sapała <kasper.sapala@gmail.com>
2019-03-16 19:23:12 +0100

    erros

--- cw02/zad1/generator.c
+++ cw02/zad1/generator.c
@@ -1,7 +1,7 @@
 #include "generator.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -26,9 +26,16 @@ int sort(unsigned char * (*read_fun)(unsigned int, unsigned int, const char * ),
     for(int i = 0; i < num_records; ++i) {
         int min_index = i;
         unsigned char* min_char = read_fun(i*byte_num, i*byte_num, file_name);
-
+        if(min_char == NULL) {
+            fprintf(stderr, "error while sorting\n");
+            return -1;
+        }
         for(int j = i+1; j < num_records; ++j) {
             unsigned char *my_char = read_fun(j*byte_num, j*byte_num, file_name);
+            if(my_char == NULL) {
+                fprintf(stderr, "error while sorting\n");
+                return -1;
+            }
             if(my_char[0] < min_char[0]) {
                 free(min_char);
                 min_char = my_char;
@@ -42,6 +49,11 @@ int sort(unsigned char * (*read_fun)(unsigned int, unsigned int, const char * ),
         unsigned char * my_record = read_fun(i*byte_num, (i+1)*byte_num-1, file_name);
         unsigned char * min_record = read_fun(min_index*byte_num, (min_index+1)*byte_num-1, file_name);
 
+        if(my_record == NULL || min_record == NULL) {
+            fprintf(stderr, "error while sorting\n");
+            return -1;
+        }
+
         write_fun(min_index*byte_num, my_record, file_name, byte_num);
         write_fun(i*byte_num, min_record, file_name, byte_num);
 
@@ -53,10 +65,15 @@ int sort(unsigned char * (*read_fun)(unsigned int, unsigned int, const char * ),
 }
 
 unsigned char * read_sys_fun(unsigned int byte_start, unsigned int byte_end, const char * file_name) {
+    int file = open(file_name, O_RDONLY); 
+
+    if(file < 0) {
+
+        return NULL;
+    }
+
     unsigned int bytes_to_read = byte_end-byte_start+1;
     unsigned char *buffer = calloc(bytes_to_read, sizeof(unsigned char));
-
-    int file = open(file_name, O_RDONLY); 
 
     lseek(file, byte_start,0);
     read(file, buffer, bytes_to_read);
@@ -76,10 +93,15 @@ void write_sys_fun (unsigned int position, unsigned char *text, const char *file
 }
 
 unsigned char * read_lib_fun(unsigned int byte_start, unsigned int byte_end, const char * file_name) {
+    FILE *file =  fopen(file_name, "r");
+
+    if(file == NULL) {
+        return NULL;
+    }
+
     unsigned int bytes_to_read = byte_end-byte_start+1;
     unsigned char *buffer = calloc(bytes_to_read, sizeof(unsigned char));
-   
-    FILE *file =  fopen(file_name, "r");
+
     fseek(file, byte_start, SEEK_SET);
 
     fread(buffer, sizeof(unsigned char), bytes_to_read, file);
@@ -103,26 +125,41 @@ void write_lib_fun (unsigned int position, unsigned char *text, const char *file
 
 
 int sort_sys(const char *file_name, const unsigned int num_records, const unsigned int byte_num) { 
-    return sort(
+    int sort_status =  sort(
         read_sys_fun,
         write_sys_fun,
         file_name,
         num_records,
         byte_num);
+        
+    if(sort_status < 0) {
+        fprintf(stderr, "error while sys sorting\n");
+    }
+    return sort_status;
 }
 
 int sort_lib(const char *file_name, const unsigned int num_records, const unsigned int byte_num) {
-     return sort(
+     int sort_status = sort(
         read_lib_fun,
         write_lib_fun,
         file_name,
         num_records,
         byte_num);
+
+
+    if(sort_status < 0) {
+        fprintf(stderr, "error while lib sorting\n");
+    }
+    return sort_status;
 }
 
 int copy_sys( const char *file_from, const char * file_to, const unsigned int num_records, const unsigned int byte_num){
     for(int i = 0; i < num_records; ++i) {
         unsigned char *my_rec = read_sys_fun(i*byte_num, (i+1)*byte_num-1, file_from);
+        if(my_rec == NULL) {
+            fprintf(stderr, "error while copying sys\n");
+            return -1;
+        }
         write_sys_fun(i*num_records, my_rec, file_to, byte_num);
         free(my_rec);
     }
@@ -132,6 +169,10 @@ int copy_sys( const char *file_from, const char * file_to, const unsigned int nu
 int copy_lib( const char *file_from, const char * file_to, const unsigned int num_records, const unsigned int byte_num){
     for(int i = 0; i < num_records; ++i) {
         unsigned char *my_rec = read_lib_fun(i*byte_num, (i+1)*byte_num-1, file_from);
+        if(my_rec == NULL) {
+            fprintf(stderr, "error while copying lib\n");
+            return -1;
+        }
         write_lib_fun(i*num_records, my_rec, file_to, byte_num);
         free(my_rec);
     }
@@ -142,6 +183,10 @@ int copy_lib( const char *file_from, const char * file_to, const unsigned int nu
 int print_file(const char *file_name, const unsigned int num_records, const unsigned int byte_num) {
     for(int i = 0; i < num_records; ++i) {
         unsigned char* my_char = read_sys_fun(i*byte_num, (i+1)*byte_num-1, file_name);
+        if(my_char == NULL) {
+            fprintf(stderr, "error while printing file\n");
+            return -1;
+        }
         printf("record: %d, is: %s, first char num: %d\n", i, my_char, (int)my_char[0]);
         free(my_char);
     }
--- cw02/zad1/generator_prog.c
+++ cw02/zad1/generator_prog.c
@@ -1,21 +1,60 @@
 #include "generator.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    generate("super.txt", 10, 10);
+int parse_generate(int argc, char *argv[], int i) {
+    char * file_name = argv[i+1];
+    int rec_num = atoi(argv[i+2]);
+    int byte_num = atoi(argv[i+3]);
+    
+    generate(file_name, rec_num, byte_num);
 
-    printf("unsorted:\n");
-    print_file("super.txt", 10, 10);
+    return i+4;
+}
 
-    copy_sys("super.txt", "super.sys.txt", 10, 10);
-    sort_sys("super.sys.txt", 10, 10);
-    printf("sys:\n");
-    print_file("super.sys.txt", 10, 10);
+int parse_sort(int argc, char *argv[], int i) {
+    char * file_name = argv[i+1];
+    int rec_num = atoi(argv[i+2]);
+    int byte_num = atoi(argv[i+3]);
+    char * lib_sys = argv[i+4];
 
+    if(!strcmp(lib_sys, "lib")) {
+        sort_lib(file_name, rec_num, byte_num);
+    } else if(!strcmp(lib_sys, "sys")) {
+        sort_sys(file_name, rec_num, byte_num);
+    }
+    return i+5;
+}
 
-    copy_lib("super.txt", "super.lib.txt", 10, 10);
-    sort_lib("super.lib.txt", 10, 10);
-    printf("lib:\n");
-    print_file("super.lib.txt", 10, 10);
+int parse_copy(int argc, char *argv[], int i) {
+    char * file_from = argv[i+1];
+    char * file_to = argv[i+2];
+    int rec_num = atoi(argv[i+3]);
+    int byte_num = atoi(argv[i+4]);
+    char * lib_sys = argv[i+5];
+
+    if(!strcmp(lib_sys, "lib")) {
+        copy_lib(file_from, file_to, rec_num, byte_num);
+    } else if(!strcmp(lib_sys, "sys")) {
+        copy_sys(file_from, file_to, rec_num, byte_num);
+    }
+    return i+6;
+}
+
+int main(int argc, char *argv[]) {
+    int i = 1;
+    while(i < argc) {
+        if(!strcmp(argv[i], "generate")) {
+            i = parse_generate(argc, argv, i);
+        } else if(!strcmp(argv[i], "sort")) {
+            i = parse_sort(argc, argv, i);
+        } else if(!strcmp(argv[i], "copy")) {
+            i = parse_copy(argc, argv, i);
+        } else {
+            fprintf(stderr,"Wrong command\n");
+            return -1;
+        }
+    }
     return 0;
 }

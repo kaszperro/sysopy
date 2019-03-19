9231c5ee7b9b299bf29db391c7b1a088dfa54f04
Kasper <kasper@desktop-gkogd7o.home>
2019-03-19 20:41:42 +0100

    cw02 update

--- cw02/zad2/files_dir.c
+++ cw02/zad2/files_dir.c
@@ -1,7 +1,14 @@
+#define _XOPEN_SOURCE 500
+
 #include "files.h"
 #include <dirent.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 int print_files(char *root_path, char mode, time_t date, int (*print_fun)( const char * , const struct stat )){
     if (root_path == NULL)
@@ -13,4 +20,48 @@ int print_files(char *root_path, char mode, time_t date, int (*print_fun)( const
         fprintf(stderr, "dir error\n");
         return -1;
     }
+    struct dirent* file;
+
+    char new_path[256];
+
+    while ((file = readdir(dir)) != NULL) {
+        strcpy(new_path, root_path);
+        strcat(new_path, "/");
+        strcat(new_path, file->d_name);
+
+        struct stat sb;      
+        
+        if (lstat(new_path, &sb) < 0) {
+            fprintf(stderr, "unable to lstat file %s", new_path);
+            return -1;
+            //perr("unable to lstat file %s", file->d_name);
+        }
+
+
+        if (S_ISDIR(sb.st_mode)) {
+            if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0) {
+                continue;
+            }
+
+            print_files(new_path, mode, date, print_fun);
+        }
+
+
+
+        time_t modif_time = sb.st_mtime;
+
+        int diff = difftime(modif_time, date);
+        
+        if (!( (diff == 0 && mode == '=')
+                || (diff > 0 && mode == '>')
+                || (diff < 0 && mode == '<') )) 
+           continue;
+
+
+        print_fun(new_path, sb);
+
+    }
+
+    return 0;
+
 }
--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -1,33 +1,37 @@
+#define _XOPEN_SOURCE 500
+
 #include "files.h"
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+
 
 const char format[] = "%Y-%m-%d %H:%M:%S";
 
 int print_from_stat(const char * filename, const struct stat statptr) {
     char file_type[64] = "undefined";
 
-    if((statptr.st_mode & S_IFMT) == S_IFREG) {     
+    if(S_ISREG(statptr.st_mode)) {     
         strcpy(file_type, "file");
     } 
-    else if((statptr.st_mode & S_IFMT)  == S_IFDIR) {
+    else if(S_ISDIR(statptr.st_mode)) {
         strcpy(file_type, "dir");
     }
-    else if((statptr.st_mode & S_IFMT) == S_IFLNK) {
+    else if(S_ISLNK(statptr.st_mode)) {
         strcpy(file_type, "slink");
     }
-    else if((statptr.st_mode & S_IFMT) == S_IFCHR) {
+    else if(S_ISCHR(statptr.st_mode)) {
         strcpy(file_type, "char dev");
     }
-    else if((statptr.st_mode & S_IFMT) == S_IFBLK) {
+    else if(S_ISBLK(statptr.st_mode)) {
         strcpy(file_type, "block dev");
     }
-    else if((statptr.st_mode & S_IFMT) == S_IFIFO) {
+    else if(S_ISFIFO(statptr.st_mode)) {
         strcpy(file_type, "fifo");
     }
-    else if((statptr.st_mode & S_IFMT) == S_IFSOCK) {
+    else if(S_ISSOCK(statptr.st_mode)) {
         strcpy(file_type, "socket");
     }
 
@@ -43,7 +47,7 @@ int print_from_stat(const char * filename, const struct stat statptr) {
     strftime(access_time_str, 255, format, &tm_access_time);
 
 
-     printf("%s || type: %s, size: %lld, modification time: %s, access time: %s\n", 
+     printf("%s || type: %s, size: %ld, modification time: %s, access time: %s\n", 
             filename, file_type, statptr.st_size, modif_time_str, access_time_str);
 
     return 0;

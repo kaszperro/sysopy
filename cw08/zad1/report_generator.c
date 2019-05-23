1948c2771331dbb9fd5878d8042807a75c6b025c
kaszperro <kasper.sapala@gmail.com>
2019-05-23 14:57:49 +0200

    report gen update

--- cw08/zad1/report_generator.c
+++ cw08/zad1/report_generator.c
@@ -36,20 +36,31 @@ int main() {
        
     }
 
+    fprintf(fp, "-=-=-=-block-=-=-=-\n");
+
+    for(int fs = 3; fs <= 65; fs += 5) {
+        fprintf(fp, "\n==================\nfilter size: %d\n------------\n", fs);
+        for(int t = 1; t <= 8; t*=2) {
+            fprintf(fp, "\tthreads: %d\n", t);
+
+            char sys_call[255];
+            sprintf(sys_call, "./main %d block %s generated_filters/%d %s", t, img_path, fs, out_path);
+
+            FILE *res = popen(sys_call, "r");
+
+            char output[1024];
+            int num_read = fread(output, sizeof(char),1024, res);
+            output[num_read] = '\0';
+            fwrite(output, sizeof(char), num_read, fp);
+
+            pclose(res);
+        }
+       fprintf(fp, "==================\n");
+       
+    }
+
 
     fclose(fp);
-
-    /*
-     for(int t = 1; t <= 8; t*=2) {
-            char filter_path[255];
-            sprintf(filter_path, "generated_filters/%d", fs);
-
-            char threads[32];
-            sprintf(threads, "%d", t);
-
-            execl("main", "main", threads, "block", img_path, filter_path, out_path, NULL);
-        }
-    */
 
     return 0;
 }
